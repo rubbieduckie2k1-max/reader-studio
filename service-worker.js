7a9f4ed94d76343cc0e1724f2ce172c8f327beb9
rubbieduckie2k1-max <rubbieduckie2k1@gmail.com>
2026-09-14 13:07:26 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'reader-studio-v12';
+const CACHE = 'reader-studio-v13';
 const LOCAL = ['./','./index.html','./styles.css','./app.js','./config.js','./vendor/jszip.min.js','./manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(LOCAL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(
